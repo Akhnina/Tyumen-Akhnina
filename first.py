c163e59d54f26d5db1f35b5a331ce4b49fc6b832
Mary <Akhnina00@mail.ru>
2021-11-19 21:30:06 +0500

    yellow circle

--- first.py
+++ first.py
@@ -1,6 +1,44 @@
-def main():
-    print('My first git-repo')
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
+import random
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('un.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(500, 500, 600, 500)
+        self.setWindowTitle('Рисование')
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        x = random.randint(0, 235)
+        y = random.randint(0, 235)
+        d = random.randint(0, 300)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(x, y, d, d)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
